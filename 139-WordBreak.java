69316ab511e6d42d3b9b9b604ba3733bc48d5c15
Eric C <37767053+echoi11@users.noreply.github.com>
2018-06-21 10:02:09 -0700

    Update 139-WordBreak.java

--- 139-WordBreak.java
+++ 139-WordBreak.java
@@ -1,12 +1,33 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        // boolean canBreak = false;
-        // for(String word : wordDict) {
-        //     if(s.startsWith(word) && s.length() > word.length()) {
-        //         return wordBreakRec(s.substring(word.length()), wordDict);
-        //     }
-        // }
-        // return false;
+        //return wordBreakRec(s, wordDict); // recursive
+        return wordBreakHash(s, wordDict);
+    }
+
+    public boolean wordBreakHash(String s, List<String> wordDict) {
+        HashSet dictSet = new HashSet();
+        for(String word : wordDict) {
+            dictSet.add(word);
+        }
+        return wordBreakHashRec(s, dictSet);
+    }
+    
+    public boolean wordBreakHashRec(String s, Set dictSet) {
+        for(int i=1; i <= s.length(); i++) {
+            if(dictSet.contains(s.substring(0,i))) {
+                if(i==s.length()) {
+                    return true;
+                } else {
+                    if(wordBreakHashRec(s.substring(i), dictSet)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    public boolean wordBreakRec(String s, List<String> wordDict) {
         boolean canBreak = false;
         for(String word : wordDict) {
             if(s.startsWith(word)) {
@@ -16,20 +37,6 @@ class Solution {
                     if(wordBreak(s.substring(word.length()), wordDict)) {
                         return true;
                     }
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean wordBreakRec(String s, List<String> wordDict) {
-        boolean canBreak = false;
-        for(String word : wordDict) {
-            if(s.startsWith(word)) {
-                if(s.length() == word.length()) {
-                    return true;
-                } else {
-                    return wordBreakRec(s.substring(word.length()), wordDict);
                 }
             }
         }
