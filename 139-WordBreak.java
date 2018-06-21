be974d5a117247d105f8284a42eb808f11d71c84
Eric C <37767053+echoi11@users.noreply.github.com>
2018-06-21 15:10:43 -0700

    Update 139-WordBreak.java

--- 139-WordBreak.java
+++ 139-WordBreak.java
@@ -1,32 +1,44 @@
 class Solution {
     
     // int[] wordLetters = new int[26];
+    int minlen = 10000;
+    int maxlen = 0;
     int[] dictLetters = new int[26];
     HashSet<String> dictSet = new HashSet<String>();
     
     public boolean wordBreak(String s, List<String> wordDict) {
-        if(!optimize(s, wordDict)) return false;
-        return wordBreakRec(s, new ArrayList<String>(dictSet));
+        if(!optimize(s, wordDict)) {
+            return false;
+        }
+   //     return wordBreakRec(s, new ArrayList<String>(dictSet));
             
 //        return wordBreakRec(s, wordDict); // recursive
-//        return wordBreakHash(s, wordDict);
+        return wordBreakHashRec(s, dictSet);
     }
 
     public boolean optimize(String s, List<String> wordDict) {
         for(String word : wordDict) {
             dictSet.add(word);
             addToLetterArray(word, dictLetters);
-        }
-        for(String word : wordDict) {
-            dictSet.remove(word);
-            System.out.println("removed:"+word);
-            if(!wordBreakHashRec(word, dictSet)) {
-                dictSet.add(word);
-                System.out.println("added:" + word);
+            if(word.length() < minlen) {
+                minlen = word.length();
+            }
+            if(word.length() > maxlen) {
+                maxlen = word.length();
             }
         }
+        System.out.println("min:max = "+minlen+":"+maxlen);
+        // for(String word : wordDict) {
+        //     dictSet.remove(word);
+        //     System.out.println("removed:"+word);
+        //     if(!wordBreakHashRec(word, dictSet)) {
+        //         dictSet.add(word);
+        //         System.out.println("added:" + word);
+        //     }
+        // }
         System.out.println("Done optimize:"+dictSet);
         if(!letterCheck(s)) return false;
+        System.out.println("LetterCheck passed.");
         return true;
         //return wordBreakHashRec(s, dictSet);
     }
@@ -49,20 +61,16 @@ class Solution {
     }
     
     public boolean wordBreakHashRec(String s, Set dictSet) {
-        for(int i=1; i <= s.length(); i++) {
+        System.out.println("checking string " + s);
+        
+        for(int i=maxlen; i > 0 && i >= minlen; i--) {
+            System.out.println("checking string " + dictSet.contains(s.substring(0,i)));
             if(dictSet.contains(s.substring(0,i))) {
                 if(i==s.length()) {
                     return true;
                 } else {
-                    String s2 = s.substring(i);
-                    for(int j=s2.length()-1; j >= 0; j--) {
-                        if(dictSet.contains(s2.substring(j))) {
-                            if(j==0) {
-                                return true;
-                            } else if(wordBreakHashRec(s2.substring(0,j), dictSet)) {
-                                return true;
-                            }
-                        }
+                    if(wordBreakHashRec(s.substring(0,i), dictSet)) {
+                        return true;
                     }
                 }
             }
@@ -78,6 +86,7 @@ class Solution {
     
     // all letters in the word must exist in dictionary letters.
     public boolean letterCheck(String s) {
+        System.out.println("dictLetters="+dictLetters);
         for(int i=0; i< s.length(); i++ ) {
             if(dictLetters[s.charAt(i)-'a'] == 0) {
                 return false;
