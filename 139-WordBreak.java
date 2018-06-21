131260288e187f04ca2f41da27c3169494825ad1
Eric C <37767053+echoi11@users.noreply.github.com>
2018-06-21 11:20:33 -0700

    Update 139-WordBreak.java

--- 139-WordBreak.java
+++ 139-WordBreak.java
@@ -1,14 +1,50 @@
 class Solution {
+    
+    // int[] wordLetters = new int[26];
+    int[] dictLetters = new int[26];
+    HashSet<String> dictSet = new HashSet<String>();
+    
     public boolean wordBreak(String s, List<String> wordDict) {
-        //return wordBreakRec(s, wordDict); // recursive
-        return wordBreakHash(s, wordDict);
+        if(!optimize(s, wordDict)) return false;
+        return wordBreakRec(s, new ArrayList<String>(dictSet));
+            
+//        return wordBreakRec(s, wordDict); // recursive
+//        return wordBreakHash(s, wordDict);
     }
 
+    public boolean optimize(String s, List<String> wordDict) {
+        for(String word : wordDict) {
+            dictSet.add(word);
+            addToLetterArray(word, dictLetters);
+        }
+        for(String word : wordDict) {
+            dictSet.remove(word);
+            System.out.println("removed:"+word);
+            if(!wordBreakHashRec(word, dictSet)) {
+                dictSet.add(word);
+                System.out.println("added:" + word);
+            }
+        }
+        System.out.println("Done optimize:"+dictSet);
+        if(!letterCheck(s)) return false;
+        return true;
+        //return wordBreakHashRec(s, dictSet);
+    }
+    
     public boolean wordBreakHash(String s, List<String> wordDict) {
         HashSet dictSet = new HashSet();
         for(String word : wordDict) {
             dictSet.add(word);
+            addToLetterArray(word, dictLetters);
         }
+        for(String word : wordDict) {
+            dictSet.remove(word);
+            if(!wordBreakHashRec(word, dictSet)) {
+                dictSet.add(word);
+            }
+        }
+        System.out.println(dictSet);
+        if(!letterCheck(s)) return false;
         return wordBreakHashRec(s, dictSet);
     }
     
@@ -18,13 +54,36 @@ class Solution {
                 if(i==s.length()) {
                     return true;
                 } else {
-                    if(wordBreakHashRec(s.substring(i), dictSet)) {
-                        return true;
+                    String s2 = s.substring(i);
+                    for(int j=s2.length()-1; j >= 0; j--) {
+                        if(dictSet.contains(s2.substring(j))) {
+                            if(j==0) {
+                                return true;
+                            } else if(wordBreakHashRec(s2.substring(0,j), dictSet)) {
+                                return true;
+                            }
+                        }
                     }
                 }
             }
         }
         return false;
+    }
+    
+    public void addToLetterArray(String s, int[] letterArray) {
+        for(int i=0; i< s.length(); i++ ) {
+            letterArray[s.charAt(i)-'a'] = 1;
+        }
+    }
+    
+    // all letters in the word must exist in dictionary letters.
+    public boolean letterCheck(String s) {
+        for(int i=0; i< s.length(); i++ ) {
+            if(dictLetters[s.charAt(i)-'a'] == 0) {
+                return false;
+            }
+        }
+        return true;
     }
     
     public boolean wordBreakRec(String s, List<String> wordDict) {
