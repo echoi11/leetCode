cdfe4658b119ade1131b89b1cbca6d4ae18d79b0
Eric C <37767053+echoi11@users.noreply.github.com>
2018-06-24 16:21:02 -0700

    Update 0853-CarFleet.java

--- 0853-CarFleet.java
+++ 0853-CarFleet.java
@@ -1,7 +1,6 @@
 class Solution {
     
     int[] fleet;
-    // int numOfFleet=0;
     
     public int carFleet(int target, int[] position, int[] speed) {
         if(position==null || position.length==0) {
@@ -20,13 +19,14 @@ class Solution {
         }
         
         initFleet(position.length);
-        System.out.println("position=" + Arrays.toString(position));
-        System.out.println("speed   =" + Arrays.toString(speed));
+        System.out.println("init position=" + Arrays.toString(position));
+        System.out.println("init speed   =" + Arrays.toString(speed));
         
         int cycles = target;
-        while(cycles >0 ) {
+        while(cycles > 0) {
             for(int i = 0; i < position.length - 1; i ++ ) {
                 if(position[i]<=target && position[i]==position[i+1]) {
+                    // combine fleets if two cars are at same position but not past destination
                     if(fleet[i] != fleet[i+1]) {
                         fleet[i+1] = fleet[i];
                         speed[i+1] = speed[i];
@@ -34,12 +34,44 @@ class Solution {
                     }
                 }
             }
-            System.out.println("position=" + Arrays.toString(position));
-            System.out.println("fleet=" + Arrays.toString(fleet));
+            System.out.println("revised fleet =" + Arrays.toString(fleet));
+            System.out.println("position      =" + Arrays.toString(position));
 
-            position[0] += speed[0];
-            for(int i = 0; i < position.length - 1; i ++ ) {
-                position[i+1] = Math.min(position[i+1] + speed[i+1], position[i]);
+            double [] timeToTarget= new double[fleet.length];
+            for(int i = 0; i < position.length; i ++ ) {
+                //System.out.println(i+":pos, speed:" + position[i] + ","+speed[i]);
+                if((position[i] < target) && (position[i]+ speed[i] >=target)) {
+                    timeToTarget[i] = 1d * (target - position[i]) / speed[i]; // 0 < n < =1
+                } else {
+                    //System.out.println((position[i] < target) + "");
+                    //System.out.println((position[i] + speed[i] >= target) + "");
+                    timeToTarget[i] =0;
+                }
+                //System.out.println("ttt="+ timeToTarget[i]);
+                if(i==0) {
+                    position[0] += speed[0];
+                } else {
+                    if(timeToTarget[i-1] > 0d) { // can this car reach target in time?
+                        //System.out.println("timeToTarget check..." + i);
+                        //System.out.println(""+(speed[i] * timeToTarget[i-1] >= (target - position[i])));
+                        if(speed[i] * timeToTarget[i-1] >= (target - position[i])) {
+                            int iToMerge = i;
+                            int thisFleet = fleet[i];
+                            int targetFleet = fleet[i-1];
+                            while(iToMerge < fleet.length) {
+                                if(fleet[iToMerge] == thisFleet) {
+                                    fleet[iToMerge] = targetFleet;
+                                    iToMerge++;
+                                } else {
+                                    break;
+                                }
+                            }
+                        } else {
+                            timeToTarget[i-1] =0;
+                        }
+                    }
+                    position[i] = Math.min(position[i] + speed[i], position[i-1]);
+                }
             }
             cycles--;
             System.out.println("position after=" + Arrays.toString(position));
