24d2018bb2f9d315b598e0e9e924a3366af8e20e
Eric C <37767053+echoi11@users.noreply.github.com>
2018-06-24 19:22:20 -0700

    Update 0856-ScoreOfParen.java

--- 0856-ScoreOfParen.java
+++ 0856-ScoreOfParen.java
@@ -1,95 +1,27 @@
 class Solution {
     
-    String S;
-    StringBuilder exp = new StringBuilder("");
+    int parens=0;
+    int ans = 0;
     
-    String res;
-
-    ArrayList <Node>l = new ArrayList<Node>();
-    
-    int result = 0;
-    int tempResult = 0;
     
     public int scoreOfParentheses(String S) {
-        this.S = S;
-        if(S==null || S.length()==0) {
+        
+        if(S==null || S.equals("")) {
             return 0;
         }
-        for(int i=0; i < S.length()-1; i++) {
-            if(S.charAt(i) == '(') {
-                if(S.charAt(i+1) =='(') {   // ((
-                    exp.append("2*(");
-                } else {                    // ()
-                    exp.append("1");
-                }
-            } else { // )
-                if(S.charAt(i+1)=='(') {
-                    exp.append("+");
-                } else { // ))
-                    exp.append(")");
+
+        for(int i=0; i < S.length(); i++) {
+            if(S.charAt(i)=='(') {
+                parens++;
+            } else {
+                parens--;
+                if(S.charAt(i-1)=='(') {
+                    ans = ans + ((int) Math.pow(2, parens));
+                    // ans = ans + (1 << parens);
                 }
             }
         }
-        System.out.println(exp);
+        return ans;
         
-        
-        Node root = new Node();
-        Node res = root;
-        Node p = res;
-        for(int i=0; i < S.length()-1; i++) {
-            if(S.charAt(i) == '(') {
-                if(S.charAt(i+1) =='(') {   // ((
-                    exp.append("2*(");
-                    p.value = 2;
-                    res = new Node();
-                    p.mult = res;
-                    res.prev = p;
-                    p = res;
-                } else {                    // ()
-                    exp.append("1");
-                    p.value = 1;
-                }
-            } else { // )
-                if(S.charAt(i+1)=='(') {    // )(
-                    exp.append("+");
-                    res = new Node();
-                    p.add = res;
-                    res.prev = p.prev;
-                    p = res;
-                } else { // ))
-                    exp.append(")");
-                    p = p.prev;
-                }
-            }
-        }
-
-        result = eval(root);
-        
-        return result;
-        
-    }
-    
-    private int eval(Node n) {
-        if(n.mult!=null) {
-            n.value = n.value * eval(n.mult);
-        }
-        if(n.add!=null) {
-            n.value = n.value + eval(n.add);
-        }
-        return n.value;
-    }
-    
-    private class Node {
-        Node prev;
-        Node mult; // for *
-        Node add; // for *
-        int value;
-        
-        public Node() {
-        }
-
-        public Node(int value) {
-            this.value = value;
-        }
     }
 }
