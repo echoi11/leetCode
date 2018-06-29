3f0626d668375f155b012e1ce80ce5329caa97e0
Eric C <37767053+echoi11@users.noreply.github.com>
2018-06-29 16:28:24 -0700

    Update and rename 0336-PalindromePairs to 0336-PalindromePairs.java

--- 0336-PalindromePairs.java
+++ 0336-PalindromePairs.java
@@ -1,9 +1,12 @@
 class Solution {
+    HashSet<String> mem = new HashSet<String>();
     public List<List<Integer>> palindromePairs(String[] words) {
         List<List<Integer>> sol = new ArrayList<List<Integer>>();
         for(int i=0; i < words.length; i++) {
             for(int j=0; j< words.length; j++) {
-                if(i != j && isPalindrome(words[i] + words[j])) {
+                if((words[i].length()==0 || words[j].length()==0 || words[i].charAt(0)==words[j].charAt(words[j].length()-1))
+                        && i != j && isPalindrome(words[i] + words[j])) {
+                // if(i != j && isPalindrome(words[i] + words[j])) {
                     List<Integer> subL = new ArrayList<Integer>();
                     subL.add(i); subL.add(j);
                     sol.add(subL); 
@@ -14,6 +17,7 @@ class Solution {
     }
                    
     private boolean isPalindrome(String s) {
+        if(mem.contains(s)) return true;
         //System.out.println("comparing=" + s);
         for(int i=0; i< (s.length()/2); i++) {
             // System.out.println(i);
@@ -22,6 +26,7 @@ class Solution {
                 return false;
             }
         }
+        mem.add(s);
         return true;
     }
 }
