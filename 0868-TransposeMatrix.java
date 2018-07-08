123db7e480c31759fc36d7426eccbba861d74962
Eric C <37767053+echoi11@users.noreply.github.com>
2018-07-08 09:46:10 -0700

    Create 0868-TransposeMatrix.java

---
+++ 0868-TransposeMatrix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] transpose(int[][] A) {
+    	int[][] B = new int[A[0].length][A.length];
+    	int tmp = 0;
+        for(int i=0; i < A.length; i++) {
+        	for(int j=0; j< A[0].length; j++) {
+                B[j][i]= A[i][j];
+        	}
+        }
+        return B;
+    }
+}
