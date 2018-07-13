bc3e839b4697a83da4bd5c3a11e2791d4e9dd900
Eric C <37767053+echoi11@users.noreply.github.com>
2018-07-13 00:57:04 -0700

    Create 0530-MinimumAbsoluteDifference.java

---
+++ 0530-MinimumAbsoluteDifference.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int getMinimumDifference(TreeNode root) {
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        int min = Integer.MAX_VALUE;
+        int prevNum = 0;
+        boolean isFirst = true;
+
+        TreeNode n = root;
+        do {
+            if(n.left != null) {
+                s.push(n);
+                n = n.left;
+            } else {
+                if(isFirst) {
+                    isFirst = false;
+                } else {
+                    min = Math.min(min, n.val - prevNum);
+                }
+                prevNum = n.val;
+                System.out.println(n.val);
+                if(n.right!=null) {
+                    n = n.right;
+                } else {
+                    if(!s.isEmpty()) {
+                        n = s.pop();
+                        n.left = null;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        } while(true);
+        return min;
+    }
+}
