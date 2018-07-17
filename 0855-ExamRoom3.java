7da8a899ad1319a5b4a58a1801a973bdded6eea1
Eric C <37767053+echoi11@users.noreply.github.com>
2018-07-16 21:35:23 -0700

    Update 0855-ExamRoom3.java

--- 0855-ExamRoom3.java
+++ 0855-ExamRoom3.java
@@ -1,110 +1,125 @@
 class ExamRoom {
 
-    // start, end indexes, inclusive and seat offset
-   // int[][] seats; //= int [N][3]
-    
-    int[] seats;
+	int[] seats;
 
-    // min dist to next student
-    // Set of open seats, sorted by min dist, then by start index.
-    TreeSet<OpenSection> ts = new TreeSet<OpenSection>();
-    // number of Seats
-    int seated = 0;
-//    int maxDist;
-//    int maxSeat;
-    int numOfSeats;
-    
-    public ExamRoom(int N) {
-        numOfSeats = N;
-        seats = new seats[N];
-    }
-    
-    public int seat() {
-        if(seated==0) {
-            OpenSection os = new OpenSection(0, N-1);
-            ts.add(os);
-            seated_++;
-            seats[0][0] = 0;
-            seats[0][1] = (N-1) *2;
-            maxDist = seats[0][1];
-            maxSeat = 0;
-        } else {
-            
-            while(true) {
-                if(seats[i][0] == maxDist) { // know maxDist => know maxDist seat too!
-                    sitdown(i, maxDist, true); // todo
-                } else if(seats[i][1] == maxDist ) {
-                    sitRight(i, maxDist, false);
-                }
-            }
-        }
-        seated++;
-    }
-    
-    private void sitdown(int neighbor, int dist, boolean isLeft) {
-        if(seats[maxSeat][0] != 0) {
-            // always sit on left side unless first seat
-            int offset = dist/2 + dist % 2;
-            seats[maxSeat - offset][0] = Math.min(maxSeat, dist/2);
-            seats[maxSeat - offset][1] = dist/2;
-        } else {
-            // sit right
-            int offset = dist/2;
-            seats[maxSeat - offset][0] = dist/2;
-            seats[maxSeat - offset][1] = Math.min(maxSeat + 1 - N , dist/2);
-        }
-        // adjust neighbors;
-        int newSeat = maxSeat - offset;
-        seats[newSeat - dist/2][1] = seats[newSeat][0];
-        if(seats[newSeat + dist/2][0] > 0) {
-            seats[newSeat + dist/2][0] = seats[newSeat][1];
-        } else {
-            seats[newSeat + dist/2 + 1][0] = seats[newSeat][1];
-        }
-        findMaxDist();
-    }
-    
-    private findMaxDist() {
-        maxDist = -1;
-        for(int i=0; i < seats.size; i++) {
-            if(seats[i][0] > maxDist) {
-                maxDist = seats[i][0];
-                maxSeat = i;
-            } else if(seats[i][1] > maxDist) {
-                maxDist = seats[i][1];
-                maxSeat = i;
-            }
-        }
-    }
-    
-    public void leave(int p) {
-        seats[p] = 0;
-        // adjust neighbors
-        
-    }
+	// Set of open seats, sorted by min dist, then by start index.
+	TreeSet<Section> ts = new TreeSet<Section>(Collections.reverseOrder());
+	// number of Seats
+	int seated = 0;
+	int numOfSeats;
 
-    private class OpenSection() {
-        int start;
-        int end;
-        int dist; // mininum distance
-        int seatOffset; // index offset for next seat.
-        
-        public OpenSection(int start, int end) {
-            this.start = start;
-            this.end = end;
-            if(start==0 && seat[0]==0) {
-                seatOffset = 0;
-                dist = end - start - 1;
-            } else if(end==numOfSeats-1 && seat[end]==0) {
-                seatOffset = end - start;
-                dist = end - start - 1;
-            } else {
-                dist = (end - start - 1) / 2;
-                seatOffset = (end - start)/2;
-            }
-            
+	public ExamRoom(int N) {
+		numOfSeats = N;
+		seats = new int[N];
+	}
+
+	public int seat() {
+		int seatNumber;
+		if (seated == 0) {
+			seatNumber = 0;
+			seats[seatNumber] = 1;
+			Section sec = new Section(0, numOfSeats - 1);
+			ts.add(sec);
+		} else {
+			Section sec = ts.first();
+            System.out.println("got sec from ts: " + sec.toString());
+			System.out.println("remove:"+ts.remove(sec));
+			seatNumber = sec.start + sec.seatOffset;
+			if(sec.seatOffset==0 || sec.seatOffset==(sec.end - sec.start)) {
+				seats[seatNumber] = 1;
+				sec = new Section(sec.start, sec.end);
+				ts.add(sec);
+			} else {
+				// split the section
+				seats[seatNumber] = 1;
+				Section sec1 = new Section(sec.start, seatNumber);
+				ts.add(sec1);
+				sec1 = new Section(seatNumber, sec.end);
+				ts.add(sec1);
+			}
+		}
+		seated++;
+		return seatNumber;
+	}
+
+	public void leave(int p) {
+		int start = 0;
+		int end = 0;
+
+		Section sec1 = null;
+
+		for(Section sec : ts) {
+			if(sec.end==p) {
+				start = sec.start;
+				end = sec.end;
+				sec1 = sec;
+			}
+		}
+		if(sec1!=null) {
+            ts.remove(sec1);
+            System.out.println("removed: " + sec1);
         }
-    }
+		for(Section sec : ts) {
+			if(sec.start == p) {
+				end = sec.end;
+				sec1 = sec;
+			}
+		}
+		if(sec1!=null) {
+            ts.remove(sec1);
+            System.out.println("removed: " + sec1);
+        }
+		seats[p]=0;
+		seated--;
+		ts.add(new Section(start, end));
+	}
+
+	private class Section implements Comparable<Section> {
+		int start;
+		int end;
+		int dist; // mininum distance
+		int seatOffset; // index offset for next seat.
+
+		public Section(int start, int end) {
+			this.start = start;
+			this.end = end;
+			if (start == 0 && seats[0] == 0) {
+				seatOffset = 0;
+				dist = end;
+			} else if (end == numOfSeats - 1 && seats[end] == 0) {
+				seatOffset = end - start;
+				dist = seatOffset - 1;
+			} else {
+				dist = ((end - start) / 2) - 1;
+				seatOffset = (end - start) / 2;
+			}
+            System.out.println("new section: " + toString());
+		}
+
+        public String toString() {
+            System.out.println("seated=" + seated);
+            System.out.println("seats=" + Arrays.toString(seats));
+            return start + "," + seatOffset + "," + end + "," + dist;
+        }
+        
+		public int compareTo(Section other) {
+			if (this.dist > other.dist) {
+				return 1;
+			} else if (this.dist == other.dist) {
+				if (this.start < other.start) {
+					return 1;
+                } else if (this.start==other.start) {
+                    return 0;
+				} else {
+					return -1;
+				}
+			} else {
+				return -1;
+			}
+		}
+	}
+	
+
 
 }
 
